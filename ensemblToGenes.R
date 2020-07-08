8979340b5e5ed298db16bd402469188f07e57a8e
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-07-08 15:35:53 -0500

    Update ensemblToGenes.R

--- ensemblToGenes.R
+++ ensemblToGenes.R
@@ -6,10 +6,10 @@ ensemblToGenes <- function(dataframe = data,
                             filters = 'ensembl_gene_id',
                             values = data[,column], 
                             mart = ensembl)
-                            
-  data <- dplyr::filter(data, column %in% ensembl$ensembl_gene_id)
+  
+  data <- dplyr::filter(data, !!as.name(column) %in% ensembl$ensembl_gene_id)
   colnames(ensembl)[1] <- column
-  data <- dplyr::full_join(data, ensembl, by = column)
+  data <- full_join(data, ensembl, by = column)
   data <- dplyr::select(data, c(external_gene_name, everything()), -column)
   data <- dplyr::rename(data, !!column := external_gene_name)
   return(data)
