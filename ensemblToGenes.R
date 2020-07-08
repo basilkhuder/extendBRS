818423d76f303d5f9cab53aada2fc00bbe4d14ab
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-07-08 15:15:40 -0500

    Create ensemblToGenes.R

---
+++ ensemblToGenes.R
@@ -0,0 +1,16 @@
+ensemblToGenes <- function(dataframe = data, 
+                           column = column) {
+  
+  ensembl <- biomaRt::useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
+  ensembl <- biomaRt::getBM(attributes = c('ensembl_gene_id','external_gene_name'),
+                            filters = 'ensembl_gene_id',
+                            values = counts$Chr, 
+                            mart = ensembl)
+                            
+  counts <- dplyr::filter(counts, Chr %in% ensembl$ensembl_gene_id)
+  colnames(ensembl)[1] <- column
+  counts <- full_join(counts, ensembl, by = column)
+  counts <- dplyr::select(counts, c(external_gene_name, everything()), -column)
+  counts <- dplyr::rename(counts, !!column := external_gene_name)
+  return(counts)
+} 
