8f2327c3ecb9bb55f96aede69c1e567d9bfa3421
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-07-09 14:28:05 -0500

    Update ensemblToGenes.R

--- ensemblToGenes.R
+++ ensemblToGenes.R
@@ -11,7 +11,6 @@ ensemblToGenes <- function(data = data,
                            column = column,
                            type = type) {
   
-  
   if(type == "gene"){ 
     
     download.file("https://basilkhuder.s3.us-east-2.amazonaws.com/ensembl_id_and_gene_names.txt", 
@@ -27,12 +26,13 @@ ensemblToGenes <- function(data = data,
     data <- dplyr::filter(data, !!as.name(column) %in% ensembl$ensembl_transcript_id)
     
   } else { 
-      stop("Type must be either transcript or gene")
+    stop("Type must be either transcript or gene")
   }
   
   colnames(ensembl)[1] <- column
-  data <- dplyr::full_join(data, ensembl, by = column)
+  data <- dplyr::inner_join(data, ensembl, by = column)
   data <- dplyr::select(data, c(external_gene_name, everything()), -column)
   data <- dplyr::rename(data, !!column := external_gene_name)
   return(data)
 } 
+
