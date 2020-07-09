c4d5af2481b74b50a9cced7f1b41197bbb66ff00
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-07-09 12:55:12 -0500

    Update ensemblToGenes.R

--- ensemblToGenes.R
+++ ensemblToGenes.R
@@ -7,14 +7,11 @@ ensemblToGenes <- function(data = data,
   
   
   if(type == "gene"){ 
+    
     download.file("https://basilkhuder.s3.us-east-2.amazonaws.com/ensembl_id_and_gene_names.txt", 
                   destfile = "ensembl_id_and_gene_names.txt")
     ensembl <- readr::read_tsv("ensembl_id_and_gene_names.txt")
     data <- dplyr::filter(data, !!as.name(column) %in% ensembl$ensembl_gene_id)
-    colnames(ensembl)[1] <- column
-    data <- dplyr::full_join(data, ensembl, by = column)
-    data <- dplyr::select(data, c(external_gene_name, everything()), -column)
-    data <- dplyr::rename(data, !!column := external_gene_name)
     
   } else if (type == "transcript"){ 
     
@@ -22,14 +19,14 @@ ensemblToGenes <- function(data = data,
                   destfile = "ensembl_transcript_id_and_gene_names.txt")
     ensembl <- readr::read_tsv("ensembl_transcript_id_and_gene_names.txt")
     data <- dplyr::filter(data, !!as.name(column) %in% ensembl$ensembl_transcript_id)
-    colnames(ensembl)[1] <- column
-    data <- dplyr::full_join(data, ensembl, by = column)
-    data <- dplyr::select(data, c(external_gene_name, everything()), -column)
-    data <- dplyr::rename(data, !!column := external_gene_name)
     
   } else { 
       stop("Type must be either transcript or gene")
   }
   
+  colnames(ensembl)[1] <- column
+  data <- dplyr::full_join(data, ensembl, by = column)
+  data <- dplyr::select(data, c(external_gene_name, everything()), -column)
+  data <- dplyr::rename(data, !!column := external_gene_name)
   return(data)
 } 
