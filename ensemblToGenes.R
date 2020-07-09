0096af1a18a333fcd8c9717a92e1ae20f705af10
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-07-09 12:44:07 -0500

    Update ensemblToGenes.R

--- ensemblToGenes.R
+++ ensemblToGenes.R
@@ -1,17 +1,35 @@
 #Takes a dataframe and column that has Ensembl IDs and returns dataframe with column as Gene IDs. If an Ensembl ID does not have an associated Gene ID, that ID row will 
-#be filtered out. 
+#be filtered out. Type must either be transcript (Ensembl transcript IDs) or genes (for gene IDs.) 
 
 ensemblToGenes <- function(data = data, 
-                           column = column) {
+                           column = column,
+                           type = type) {
   
-  download.file("https://basilkhuder.s3.us-east-2.amazonaws.com/ensembl_id_and_gene_names.txt", 
-                destfile = "ensembl_id_and_gene_names.txt")
   
-  ensembl <- readr::read_tsv("ensembl_id_and_gene_names.txt")
-  data <- dplyr::filter(data, !!as.name(column) %in% ensembl$ensembl_gene_id)
-  colnames(ensembl)[1] <- column
-  data <- dplyr::full_join(data, ensembl, by = column)
-  data <- dplyr::select(data, c(external_gene_name, everything()), -column)
-  data <- dplyr::rename(data, !!column := external_gene_name)
+  if(type == "gene"){ 
+    download.file("https://basilkhuder.s3.us-east-2.amazonaws.com/ensembl_id_and_gene_names.txt", 
+                  destfile = "ensembl_id_and_gene_names.txt")
+    ensembl <- readr::read_tsv("ensembl_id_and_gene_names.txt")
+    data <- dplyr::filter(data, !!as.name(column) %in% ensembl$ensembl_gene_id)
+    colnames(ensembl)[1] <- column
+    data <- dplyr::full_join(data, ensembl, by = column)
+    data <- dplyr::select(data, c(external_gene_name, everything()), -column)
+    data <- dplyr::rename(data, !!column := external_gene_name)
+    
+  } else if (type == "transcript"){ 
+    
+    download.file("https://basilkhuder.s3.us-east-2.amazonaws.com/ensembl_transcript_id_and_gene_names.txt", 
+                  destfile = "ensembl_transcript_id_and_gene_names.txt")
+    ensembl <- readr::read_tsv("ensembl_transcript_id_and_gene_names.txt")
+    data <- dplyr::filter(data, !!as.name(column) %in% ensembl$ensembl_transcript_id)
+    colnames(ensembl)[1] <- column
+    data <- dplyr::full_join(data, ensembl, by = column)
+    data <- dplyr::select(data, c(external_gene_name, everything()), -column)
+    data <- dplyr::rename(data, !!column := external_gene_name)
+    
+  } else { 
+      stop("Type must be either transcript or gene")
+  }
+  
   return(data)
 } 
