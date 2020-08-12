d81db0c1aceeac0419ff2e98c7c789463254b1e7
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-08-12 09:37:19 -0500

    Update fictitiousSeurat.R

--- fictitiousSeurat.R
+++ fictitiousSeurat.R
@@ -1,10 +1,9 @@
 #' Creates a fictitious Seurat object based upon a set number of genes and barcodes
 #' @param num.genes Number of genes to use
 #' @param num.barcodes Number of barcodes to use
-#' @param probs.zero Probabiltiy of getting a zero count amongst the genes
 #' @return A Seurat object
 #' @examples
-#' fictitiousSeurat(num.genes = 1000, num.barcodes = 1000, probs.zero = NULL)
+#' fictitiousSeurat(num.genes = 1000, num.barcodes = 1000)
 #' @export
 
 fictitiousSeurat <- function(num.genes, num.barcodes, probs.zero = NULL) { 
