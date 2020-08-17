076a1e245ae0ba583e8dba921328e83a2e25ca06
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-08-17 16:27:03 -0500

    Update exprFuncs.R

--- exprFuncs.R
+++ exprFuncs.R
@@ -11,7 +11,7 @@ cpmGeneExpr <- function(counts, gene.col, goi) {
   counts <- tibble::column_to_rownames(counts, var = gene.col)
   counts <- edgeR::cpm(counts, log = TRUE)
   counts <- tibble::as_tibble(counts, rownames = gene.col)
-  counts <- dplyr::filter(counts,!!as.name(gene.col) %in% c(goi))
+  counts <- dplyr::filter(counts, !!as.name(gene.col) %in% c(goi))
   counts <- tidyr::pivot_longer(counts,cols = -all_of(gene.col),names_to = "Samples",
                                 values_to = "CPM")
   return(counts)
