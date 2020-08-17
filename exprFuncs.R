121228656b35305bce250c2d4c95926023d76dd1
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-08-17 14:00:34 -0500

    Update exprFuncs.R

--- exprFuncs.R
+++ exprFuncs.R
@@ -1,3 +1,12 @@
+#' Calculate CPM values for a tibble of counts and return specific genes only
+#' @param counts Counts as tibble (gene or transcript ID in a column) 
+#' @param gene.col The name of the column that has the gene names or transcript IDs
+#' @param goi Genes interested in finding CPM values for 
+#' @return A tibble with CPM for genes of interest
+#' @examples
+#' cpmGeneExpr(counts, gene.col = "Genes", goi = c("Gene_1","Gene_2"))
+#' @export
+
 cpmGeneExpr <- function(counts, gene.col, goi) { 
   counts <- tibble::column_to_rownames(counts, var = gene.col)
   counts <- edgeR::cpm(counts, log = TRUE)
