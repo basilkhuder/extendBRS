0d761fbaee68bbafa342cc7bb1a870bb60cc5179
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-08-17 19:17:57 -0500

    Update exprFuncs.R

--- exprFuncs.R
+++ exprFuncs.R
@@ -2,6 +2,7 @@
 #' @param counts Counts as tibble (gene or transcript ID in a column) 
 #' @param gene.col The name of the column that has the gene names or transcript IDs
 #' @param goi Genes interested in finding CPM values for 
+#' @param log Whether you want logCPM or not. Default is TRUE
 #' @return A tibble with CPM for genes of interest
 #' @examples
 #' cpmGeneExpr(counts, gene.col = "Genes", goi = c("Gene_1","Gene_2"))
