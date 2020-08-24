bd504be8736902f1c2ef527e5bc886effb7d02b0
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-08-24 11:17:32 -0500

    Update exprFuncs.R

--- R/exprFuncs.R
+++ R/exprFuncs.R
@@ -1,6 +1,6 @@
-#' Calculate CPM values for a tibble of counts and return specific genes only
+#' Calculate CPM values for a data.frame of counts and return specific genes only
 #' @param counts Counts as tibble (gene or transcript ID in a column) 
-#' @param gene.col The name of the column that has the gene names or transcript IDs
+#' @param gene.col The name of the column that has the gene names or transcript IDs. If genes are rownames, use gene.col = "rownames"
 #' @param goi Genes interested in finding CPM values for 
 #' @param log Whether you want logCPM or not. Default is TRUE
 #' @return A tibble with CPM for genes of interest
@@ -8,7 +8,11 @@
 #' cpmGeneExpr(counts, gene.col = "Genes", goi = c("Gene_1","Gene_2"))
 #' @export
 
-cpmGeneExpr <- function(counts, gene.col, goi, log = TRUE) { 
+cpmGeneExpr <- function(counts, ...) { 
+  UseMethod("cpmGeneExpr")
+  } 
+
+cpmGeneExpr.tbl_df <- function(counts, gene.col, goi, log = TRUE) { 
   counts <- tibble::column_to_rownames(counts, var = gene.col)
   if(isTRUE(log)){ 
     counts <- edgeR::cpm(counts, log = TRUE) 
@@ -21,4 +25,42 @@ cpmGeneExpr <- function(counts, gene.col, goi, log = TRUE) {
                                 values_to = "CPM")
   return(counts)
 } 
+
+cpmGeneExpr.data.frame <- function(counts, gene.col, goi, log = TRUE) { 
+  if(!identical(gene.col, "rownames")) { 
+    rownames(counts) <- magrittr::extract2(counts, gene.col) 
+    counts <- dplyr::select(counts, -gene.col)
+    } 
+  
+  if(isTRUE(log)){ 
+    counts <- edgeR::cpm(counts, log = TRUE) 
+  } else { 
+        counts <- edgeR::cpm(counts, log = FALSE)
+  } 
+  
+  if(identical(gene.col, "rownames")) { 
+    return(counts[goi,])
+    } 
+  
+  counts <- tibble::as_tibble(counts, rownames = gene.col)
+  counts <- dplyr::filter(counts, !!as.name(gene.col) %in% goi)
+  counts <- tidyr::pivot_longer(counts,cols = -all_of(gene.col), names_to = "Samples",
+                                values_to = "CPM")
+  return(counts)
+} 
+
+cpmGeneExpr.matrix <- function(counts, gene.col, goi, log = TRUE) { 
+  
+  if(isTRUE(log)){ 
+    counts <- edgeR::cpm(counts, log = TRUE) 
+  } else { 
+        counts <- edgeR::cpm(counts, log = FALSE)
+  } 
+  counts <- tibble::as_tibble(counts, rownames = gene.col)
+  counts <- dplyr::filter(counts, !!as.name(gene.col) %in% goi)
+  counts <- tidyr::pivot_longer(counts,cols = -all_of(gene.col), names_to = "Samples",
+                                values_to = "CPM")
+  return(counts)
+} 
+
 
