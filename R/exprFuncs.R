30fd25be84245b1058e6ac6daca188141e0972f0
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-09-18 17:05:09 -0500

    Update exprFuncs.R

--- R/exprFuncs.R
+++ R/exprFuncs.R
@@ -59,4 +59,52 @@ cpmGeneExpr.matrix <- function(counts, gene.col, goi, log = TRUE) {
   return(counts[goi,])
 } 
 
+filterByExprAcross <- function(counts.list, 
+                                groups.list, 
+                                min.counts = 10, 
+                                calc.norm.factors = FALSE){ 
+  
+  gene.col <- purrr::map(counts.list, ~ colnames(.x)[1])
+  med.lib.size <- purrr::imap(counts.list, ~ select(.x, -gene.col[[.y]]))
+  med.lib.size <- purrr::map(med.lib.size, colSums)
+  med.lib.size <- purrr::map(med.lib.size, median)
+  min.counts.cpm <- purrr::imap(med.lib.size, ~ min.counts / .x * 1e6)
+  cpm.counts <- purrr::imap(counts.list, ~ column_to_rownames(.x, var = gene.col[[.y]]))
+  cpm.counts <- purrr::map(cpm.counts, ~ edgeR::cpm(.x, log = FALSE)) 
+  cpm.counts <- purrr::imap(cpm.counts, ~ tibble::as_tibble(.x, rownames = gene.col[[.y]]))
+  min.group <- purrr::map(groups.list, ~ min(table(.x)))
+  
+  filtered.genes <- purrr::imap(cpm.counts, ~summarize.func(.x, gene.col[[.y]], min.counts.cpm[[.y]])) 
+  filtered.genes <- purrr::reduce(filtered.genes, ~full_join(.x,.y,by = "Genes"))
+  filtered.genes <- dplyr::mutate(filtered.genes, 
+                                  result = if_else(test$sum.x < min.group[[1]] | test$sum.y < min.group[[2]],
+                                                   "Filter",
+                                                   "Keep")) 
+  
+  filtered.genes <- dplyr::filter(filtered.genes, result == "Keep")
+  filtered.genes <- dplyr::pull(filtered.genes, Genes)
+  
+  counts.list <- purrr::imap(counts.list, ~ dplyr::filter(.x, !!as.name(gene.col[[.y]]) %in% filtered.genes))
+  counts.list <- purrr::imap(counts.list, ~ tibble::column_to_rownames(.x, var = gene.col[[.y]]))
+  counts.list <- purrr::imap(counts.list, ~ edgeR::DGEList(.x, group = groups.list[[.y]]))
+  
+  if(isTRUE(calc.norm.factors)){
+    counts.list <- dplyr::map(counts.list, edgerR::calcNormFactors)
+  }
+  
+  return(counts.list)
+}
+
+
+summarizeFunc <- function(df, 
+                          gene.col, 
+                          min.counts){ 
+  filtered.genes <- dplyr::summarize(df, across(-!!as.name(gene.col), ~.x >= min.counts)) 
+  filtered.genes <- dplyr::mutate(filtered.genes, 
+                                  Genes = magrittr::extract2(df, gene.col),
+                                  sum = rowSums(filtered.genes))
+  filtered.genes <- dplyr::select(filtered.genes, c(Genes, sum))
+  return(filtered.genes)
+  
+}
 
